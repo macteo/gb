8824fdab9bcc7e01951f552683ab4dac6909c21c
Matteo Gavagnin <matteo.gavagnin@dimension.it>
2014-02-16 14:40:23 +0100

    Fixed image url

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "/images/fallback/" + [model.class.to_s.underscore, version_name, "default.png"].compact.join('_')
+    "uploads/" + [model.class.to_s.underscore, version_name, "default.png"].compact.join('_')
   end
 
   # Create different versions of your uploaded files:
